80123830520fc117732c3f9d5be5c382933d9b5e
Thomas Stangl <t.stangl1@oth-aw.de>
2020-05-23 17:30:30 +0200

    Entwicklungsumgebung einrichten

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <main>
+            <router-view />
+        </main>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'App',
+        data() {
+            return {};
+        },
+        components: {},
+    })
+</script>
+
+<style>
+
+</style>
