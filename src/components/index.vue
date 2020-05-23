2802ef8bbc95283dd24675368d05207f103b0811
Thomas Stangl <t.stangl1@oth-aw.de>
2020-05-23 17:30:30 +0200

    Entwicklungsumgebung einrichten

---
+++ src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ $t('FireForceDefense') }}</h1>
+        <p>{{ $t('todo:nickname-input') }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        data() {
+            return {}
+        },
+        methods: {},
+        components: {},
+    })
+</script>
