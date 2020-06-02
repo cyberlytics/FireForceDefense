00fa87d8c053023f4425bffc77fe6cb367718e74
7a5b <p.stangl1@oth-aw.de>
2020-06-02 09:27:44 +0200

    Get instance of levelManager upon world construction.

--- src/model/World.ts
+++ src/model/World.ts
@@ -2,4 +2,9 @@ import LevelManager from './LevelManager';
 import User from './User';
 
 export default class World {
+    private levelManager: LevelManager;
+
+    constructor() {
+        this.levelManager = LevelManager.getInstance();
+    }
 }
