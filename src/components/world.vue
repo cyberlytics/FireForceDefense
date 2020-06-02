765960b3bcfff4562a5c9075b458129bf8f82a67
7a5b <p.stangl1@oth-aw.de>
2020-06-02 10:21:48 +0200

    Init world component

---
+++ src/components/world.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script lang="ts">
+    import { router } from '../index';
+    import World from '../model/World';
+
+    export default {
+        data () {
+            let world: World;
+            try {
+                world = new World();
+            } catch (e) {
+                router.push('/');
+            }
+            return {};
+        },
+        components: {},
+        methods: {}
+    }
+</script>
