82ea5bb8ea0fa6fb93e0c5c78d39072eedf2b304
Thomas Stangl <t.stangl1@oth-aw.de>
2020-06-08 05:55:15 +0200

    Platzierung der Basis

--- src/components/contentPatterns.vue
+++ src/components/contentPatterns.vue
@@ -1,6 +1,6 @@
 <template>
     <defs>
-        <pattern v-for="content in ['Drehleiter', 'Loeschschiff', 'Baumgruppe', 'Steinbrocken']"
+        <pattern v-for="content in ['Drehleiter', 'Loeschschiff', 'Baumgruppe', 'Steinbrocken', 'Basis', 'Haus']"
             v-bind:id="'content-' + content" width="1" height="1" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
             <image x="0" y="0" v-bind:xlink:href="'/assets/content-' + content + '.svg'" width="100" height="100" />
         </pattern>
