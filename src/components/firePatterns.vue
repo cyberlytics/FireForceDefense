a193e3006924e5677a5b9d2ba2bb720792b0f35b
Thomas Stangl <t.stangl1@oth-aw.de>
2020-06-10 07:45:18 +0200

    Visualisierung der Feuer-Intensität
    Former-commit-id: 2d2d21e7c7391cf08f846b1b3979857b8ae56ade

---
+++ src/components/firePatterns.vue
@@ -0,0 +1,20 @@
+<template>
+    <defs>
+        <pattern v-for="category in ['1', '2', '3', '4']"
+            v-bind:id="'fire-' + category" width="1" height="1" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+            <image x="0" y="0" v-bind:xlink:href="'/assets/fire_intensity' + category + '.svg'" width="100" height="100" />
+        </pattern>
+    </defs>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        data() {
+            return {}
+        },
+        methods: {},
+        components: {},
+    })
+</script>
--- src/model/LevelMap.ts
+++ src/model/LevelMap.ts
@@ -1,6 +1,7 @@
 import type CellDefinition from './CellDefinition';
 import type Cell from './Cell';
 import HexPosition from './HexPosition';
+import FireIntensity from './FireIntensity';
 import See from '../cells/See';
 
 export default class LevelMap {
@@ -26,6 +27,15 @@ export default class LevelMap {
         setTimeout(() => {
             this.cells[0][-1] = new See(new HexPosition(-1, 0));
         }, 5000);
+
+        // TODO Remove; this is just to show the fire intensities.
+        setTimeout(() => {
+            this.getCellAt(new HexPosition(0, -4)).fireIntensity = FireIntensity.INTENSITY_0;
+            this.getCellAt(new HexPosition(1, -4)).fireIntensity = FireIntensity.INTENSITY_1;
+            this.getCellAt(new HexPosition(2, -4)).fireIntensity = FireIntensity.INTENSITY_4;
+            this.getCellAt(new HexPosition(3, -4)).fireIntensity = FireIntensity.INTENSITY_7;
+            this.getCellAt(new HexPosition(4, -4)).fireIntensity = FireIntensity.INTENSITY_10;
+        }, 3000);
     }
 
     getCellAt(pos: HexPosition): Cell|null {
