bdf36002b8744224b82a772ec25259b0aa3e4c7e
Philipp Stangl <p.stangl1@oth-aw.de>
2020-06-24 13:02:36 +0200

    Level definitions

--- src/levels/lvl001.ts
+++ src/levels/lvl001.ts
@@ -2,101 +2,102 @@ import type LevelDefinition from '../model/LevelDefinition';
 import Wiese from '../cells/Wiese';
 import HexPosition from '../model/HexPosition';
 import Steinbrocken from '../contents/Steinbrocken';
-import Feuer1 from '../effects/Feuer1';
 import See from '../cells/See';
 import Baumgruppe from '../contents/Baumgruppe';
 import Haus from '../contents/Haus';
-import Abgebrannt from '../cells/Abgebrannt';
 import Feld from '../cells/Feld';
-import Gebirge from '../cells/Gebirge';
-import Lavasee from '../cells/Lavasee';
-import Vulkangestein from '../cells/Vulkangestein';
 import Waldboden from '../cells/Waldboden';
-import Wueste from '../cells/Wueste';
 import Gewitter from '../effects/Gewitter';
 import Regen from '../effects/Regen';
-import Feuerball from '../effects/Feuerball';
-
 
 const lvl001: LevelDefinition = {
     cellDefinitions: [
-        { cellType: Wiese, pos: new HexPosition(0, 0) }, // ring 0
-        { cellType: Wiese, pos: new HexPosition(1, 0) }, // ring 1
+        /* Ring 0 */
+        { cellType: Wiese, pos: new HexPosition(0, 0) },
+        /* Ring 1 */
+        { cellType: Wiese, pos: new HexPosition(1, 0) },
         { cellType: Wiese, pos: new HexPosition(0, 1) },
         { cellType: Wiese, pos: new HexPosition(-1, 1) },
         { cellType: Wiese, pos: new HexPosition(-1, 0) },
         { cellType: Wiese, pos: new HexPosition(0, -1) },
         { cellType: Wiese, pos: new HexPosition(1, -1) },
-        { cellType: Wiese, pos: new HexPosition(2, 0) }, // ring 2
+        /* Ring 2 */
+        { cellType: Wiese, pos: new HexPosition(2, 0) },
         { cellType: Wiese, pos: new HexPosition(1, 1) },
         { cellType: Wiese, pos: new HexPosition(0, 2) },
         { cellType: Wiese, pos: new HexPosition(-1, 2) },
         { cellType: Wiese, pos: new HexPosition(-2, 2) },
-        { cellType: Wiese, pos: new HexPosition(-2, 1) },
-        { cellType: Wiese, pos: new HexPosition(-2, 0) },
-        { cellType: Wiese, pos: new HexPosition(-1, -1) },
+        { cellType: See, pos: new HexPosition(-2, 1) },
+        { cellType: See, pos: new HexPosition(-2, 0) },
+        { cellType: See, pos: new HexPosition(-1, -1) },
         { cellType: Wiese, pos: new HexPosition(0, -2) },
         { cellType: Wiese, pos: new HexPosition(1, -2) },
         { cellType: Wiese, pos: new HexPosition(2, -2) },
         { cellType: Wiese, pos: new HexPosition(2, -1) },
-        { cellType: Abgebrannt, pos: new HexPosition(3, 0) }, // ring 3
+        /* Ring 3 */
+        { cellType: Feld, pos: new HexPosition(3, 0) },
         { cellType: Feld, pos: new HexPosition(2, 1) },
-        { cellType: Gebirge, pos: new HexPosition(1, 2) },
-        { cellType: Lavasee, pos: new HexPosition(0, 3) },
-        { cellType: See, pos: new HexPosition(-1, 3) },
-        { cellType: Vulkangestein, pos: new HexPosition(-2, 3) },
+        { cellType: Wiese, pos: new HexPosition(1, 2) },
+        { cellType: Waldboden, pos: new HexPosition(0, 3) },
+        { cellType: Waldboden, pos: new HexPosition(-1, 3) },
+        { cellType: Waldboden, pos: new HexPosition(-2, 3) },
         { cellType: Waldboden, pos: new HexPosition(-3, 3) },
-        { cellType: Wiese, pos: new HexPosition(-3, 2) },
-        { cellType: Wueste, pos: new HexPosition(-3, 1) },
+        { cellType: See, pos: new HexPosition(-3, 2) },
+        { cellType: See, pos: new HexPosition(-3, 1) },
         { cellType: See, pos: new HexPosition(-3, 0) },
         { cellType: See, pos: new HexPosition(-2, -1) },
-        { cellType: Wiese, pos: new HexPosition(-1, -2) },
-        { cellType: Wiese, pos: new HexPosition(0, -3) },
-        { cellType: Wiese, pos: new HexPosition(1, -3) },
-        { cellType: Wiese, pos: new HexPosition(2, -3) },
-        { cellType: Wiese, pos: new HexPosition(3, -3) },
+        { cellType: See, pos: new HexPosition(-1, -2) },
+        { cellType: Waldboden, pos: new HexPosition(0, -3) },
+        { cellType: Waldboden, pos: new HexPosition(1, -3) },
+        { cellType: Waldboden, pos: new HexPosition(2, -3) },
+        { cellType: Waldboden, pos: new HexPosition(3, -3) },
         { cellType: Wiese, pos: new HexPosition(3, -2) },
-        { cellType: Wiese, pos: new HexPosition(3, -1) },
-        { cellType: Wiese, pos: new HexPosition(4, 0) }, // ring 4
-        { cellType: Wiese, pos: new HexPosition(3, 1) },
-        { cellType: Wiese, pos: new HexPosition(2, 2) },
+        { cellType: Feld, pos: new HexPosition(3, -1) },
+        /* Ring 4 */
+        { cellType: Feld, pos: new HexPosition(4, 0) },
+        { cellType: Feld, pos: new HexPosition(3, 1) },
+        { cellType: Feld, pos: new HexPosition(2, 2) },
         { cellType: Wiese, pos: new HexPosition(1, 3) },
-        { cellType: Wiese, pos: new HexPosition(0, 4) },
-        { cellType: Wiese, pos: new HexPosition(-1, 4) },
-        { cellType: Wiese, pos: new HexPosition(-2, 4) },
-        { cellType: Wiese, pos: new HexPosition(-3, 4) },
-        { cellType: Wiese, pos: new HexPosition(-4, 4) },
-        { cellType: Wiese, pos: new HexPosition(-4, 3) },
-        { cellType: Wiese, pos: new HexPosition(-4, 2) },
-        { cellType: Wiese, pos: new HexPosition(-4, 1) },
+        { cellType: Waldboden, pos: new HexPosition(0, 4) },
+        { cellType: Waldboden, pos: new HexPosition(-1, 4) },
+        { cellType: Waldboden, pos: new HexPosition(-2, 4) },
+        { cellType: Waldboden, pos: new HexPosition(-3, 4) },
+        { cellType: Waldboden, pos: new HexPosition(-4, 4) },
+        { cellType: See, pos: new HexPosition(-4, 3) },
+        { cellType: See, pos: new HexPosition(-4, 2) },
+        { cellType: See, pos: new HexPosition(-4, 1) },
         { cellType: See, pos: new HexPosition(-4, 0) },
         { cellType: See, pos: new HexPosition(-3, -1) },
         { cellType: See, pos: new HexPosition(-2, -2) },
-        { cellType: Wiese, pos: new HexPosition(-1, -3) },
-        { cellType: Wiese, pos: new HexPosition(0, -4) },
-        { cellType: Wiese, pos: new HexPosition(1, -4) },
-        { cellType: Wiese, pos: new HexPosition(2, -4) },
-        { cellType: Wiese, pos: new HexPosition(3, -4) },
-        { cellType: Wiese, pos: new HexPosition(4, -4) },
+        { cellType: See, pos: new HexPosition(-1, -3) },
+        { cellType: Waldboden, pos: new HexPosition(0, -4) },
+        { cellType: Waldboden, pos: new HexPosition(1, -4) },
+        { cellType: Waldboden, pos: new HexPosition(2, -4) },
+        { cellType: Waldboden, pos: new HexPosition(3, -4) },
+        { cellType: Waldboden, pos: new HexPosition(4, -4) },
         { cellType: Wiese, pos: new HexPosition(4, -3) },
-        { cellType: Wiese, pos: new HexPosition(4, -2) },
-        { cellType: Wiese, pos: new HexPosition(4, -1) },
+        { cellType: Feld, pos: new HexPosition(4, -2) },
+        { cellType: Feld, pos: new HexPosition(4, -1) },
     ],
     contentDefinitions: [
-        { contentType: Steinbrocken, pos: new HexPosition(0, 0) },
-        { contentType: Steinbrocken, pos: new HexPosition(-2, -2) }, // Should not be placed because it's on a lake
-        { contentType: Baumgruppe, pos: new HexPosition(1, 0) },
-        { contentType: Haus, pos: new HexPosition(3, 0) },
-        { contentType: Haus, pos: new HexPosition(0, -3) },
+        { contentType: Steinbrocken, pos: new HexPosition(2, -2) },
+        { contentType: Steinbrocken, pos: new HexPosition(0, -4) },
+        { contentType: Steinbrocken, pos: new HexPosition(-2, 4) },
+        { contentType: Baumgruppe, pos: new HexPosition(-3, 4) },
+        { contentType: Baumgruppe, pos: new HexPosition(-1, 4) },
+        { contentType: Baumgruppe, pos: new HexPosition(1, -4) },
+        { contentType: Baumgruppe, pos: new HexPosition(2, -4) },
+        { contentType: Baumgruppe, pos: new HexPosition(3, -4) },
+        { contentType: Baumgruppe, pos: new HexPosition(4, -4) },
+        { contentType: Haus, pos: new HexPosition(-2, 3) },
+        { contentType: Haus, pos: new HexPosition(-1, 3) },
     ],
     effectDefinitions: [
-        { effectType: Feuer1, pos: new HexPosition(0, 0), mustBeExecuted: (step) => step === 0 },
-        { effectType: Gewitter, pos: new HexPosition(-3, 0), mustBeExecuted: (step) => step % 7 === 0 },
-        { effectType: Regen, pos: new HexPosition(2, -2), mustBeExecuted: (step) => step % 20 === 0 },
-        { effectType: Feuerball, pos: new HexPosition(-4, 4), mustBeExecuted: (step) => step % 15 === 0 },
+        { effectType: Gewitter, pos: new HexPosition(3, 0), mustBeExecuted: (step) => step === 0 },
+        { effectType: Regen, pos: new HexPosition(2, -2), mustBeExecuted: (step) => step % 12 === 0 },
     ],
     levelID: 'lvl001',
-    creditStartingAmount: 100,
+    creditStartingAmount: 150,
 };
 
 export default lvl001;
