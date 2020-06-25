9b9248038ee66483dde8f9bf3da33a82c4ac8e26
Thomas Stangl <t.stangl1@oth-aw.de>
2020-06-25 19:06:15 +0200

    Preview-Cursor verbessern
    Außerdem: Optisches Feedback für Sidebar-Button-Klicks
    
    
    Former-commit-id: fc6ecb962e98d4a24677041afbd572b7594380a2

--- src/components/previewCursor.vue
+++ src/components/previewCursor.vue
@@ -1,8 +1,15 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-         v-if="contentToBuild !== null || removeMode"
-         width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet"
-         class="position-absolute" v-bind:style="{left: (mouseX-50) + 'px', top: (mouseY-50) + 'px'}" style="opacity: .7; pointer-events: none">
+    <svg
+        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        v-if="contentToBuild !== null || removeMode"
+        width="100px"
+        height="100px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid meet"
+        class="position-absolute"
+        v-bind:style="{left: (mouseX-50) + 'px', top: (mouseY-50) + 'px'}"
+        style="opacity: .7; pointer-events: none"
+    >
         <rect v-if="contentToBuild !== null" v-bind:fill="'url(#content-' + new contentToBuild().id + ')'"
               height="100" width="100" x="0" y="0" />
         <rect v-else-if="removeMode" v-bind:fill="'url(#utility-Abbauen)'"
