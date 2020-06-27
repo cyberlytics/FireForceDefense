e4fa84c341f44b8ce2b57285805c6563982a3603
Thomas Stangl <t.stangl1@oth-aw.de>
2020-06-27 10:32:41 +0200

    Lava-Effekt optimieren

--- src/components/effectPatterns.vue
+++ src/components/effectPatterns.vue
@@ -21,8 +21,8 @@
             <feGaussianBlur stdDeviation="2" />
         </filter>
         <filter id="lava-filter-drop">
-            <feTurbulence type="turbulence" baseFrequency="0.15"
-                          numOctaves="3" result="turbulence"/>
+            <feTurbulence type="fractalNoise" baseFrequency="0.15"
+                          numOctaves="3" result="turbulence" />
             <feDisplacementMap in2="turbulence" in="SourceGraphic"
                                scale="50" xChannelSelector="R" yChannelSelector="G"/>
         </filter>
@@ -30,7 +30,12 @@
             <use xlink:href="#animation-effect-Lava" x="-40" y="-30" width="180" height="180" />
         </pattern>
         <pattern id="lava-drops" width="1" height="1" viewBox="-50 -50 100 100" preserveAspectRatio="xMidYMid slice">
-            <circle v-for="dir in [0,1,2,3,4,5]" fill="#fff" v-bind:cx="getX(dir, 25)" v-bind:cy="getY(dir, 25)" r="7" filter="url(#lava-filter-drop)" />
+            <circle
+                cx="0" cy="0" r="47"
+                fill="transparent"
+                stroke="#fff" stroke-width="5" stroke-dasharray="10 30" stroke-linecap="round"
+                filter="url(#lava-filter-drop)"
+            />
         </pattern>
 
         <pattern id="smoke" width="1" height="1" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice">
@@ -53,17 +58,12 @@
         </pattern>
 
         <pattern id="effect-Lava" width="1" height="1" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice">
-            <g filter="url(#lava-filter-extend)">
-                <rect fill="url(#lava-animation)" x="0" y="00" width="100" height="100" />
-            </g>
+            <rect fill="url(#lava-animation)" x="0" y="00" width="100" height="100" filter="url(#lava-filter-extend)" />
             <mask id="lava-mask" x="0" y="0" width="100" height="100">
                 <g transform="translate(50,50)">
                     <circle class="lava-pulse" cx="0" cy="0" r="50" fill="url(#lava-drops)" />
-                    <g transform="rotate(30)">
-                        <circle class="lava-pulse" cx="0" cy="0" r="50" fill="url(#lava-drops)" />
-                    </g>
                 </g>
-                <rect fill="url(#lava-animation)" x="0" y="00" width="100" height="100" />
+                <rect fill="url(#lava-animation)" x="0" y="00" width="100" height="100" filter="url(#lava-filter-mask)" fill-opacity=".7" />
             </mask>
             <image x="0" y="0" v-bind:xlink:href="'/assets/lava.svg'" width="100" height="100" mask="url(#lava-mask)" />
         </pattern>
@@ -84,20 +84,12 @@
 
 <script lang="ts">
     import Vue from 'vue';
-    import HexPosition from '../model/HexPosition';
 
     export default Vue.extend({
         data() {
             return {}
         },
-        methods: {
-            getX: function (dir: number, size = 50) {
-                return HexPosition.getDirectionVector(dir).getCenterX(size);
-            },
-            getY: function (dir: number, size = 50) {
-                return HexPosition.getDirectionVector(dir).getCenterY(size);
-            }
-        },
+        methods: {},
         components: {
         },
     })
