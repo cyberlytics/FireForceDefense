f5cd972e64ea42474dc3a8c1f50c2975114060ff
7a5b <p.stangl1@oth-aw.de>
2020-06-28 11:33:57 +0200

    level balancing

--- src/levels/lvl004.ts
+++ src/levels/lvl004.ts
@@ -11,6 +11,8 @@ import Regen from '../effects/Regen';
 import Feuerball from '../effects/Feuerball';
 import Haus from '../contents/Haus';
 import Lava from '../effects/Lava';
+import Totholz from '../contents/Totholz';
+import Feuer3 from '../effects/Feuer3';
 
 const lvl004: LevelDefinition = {
     cellDefinitions: [
@@ -88,16 +90,32 @@ const lvl004: LevelDefinition = {
         { contentType: Steinbrocken, pos: new HexPosition(-2, 0) },
         { contentType: Haus, pos: new HexPosition(-4, 1) },
         { contentType: Haus, pos: new HexPosition(-2, 3) },
+        { contentType: Totholz, pos: new HexPosition(1, 1) },
+        { contentType: Totholz, pos: new HexPosition(1, -3) },
+        { contentType: Totholz, pos: new HexPosition(4, -2) },
+        { contentType: Totholz, pos: new HexPosition(3, 1) },
+        { contentType: Totholz, pos: new HexPosition(1, 2) },
+        { contentType: Totholz, pos: new HexPosition(-3, 1) },
+        { contentType: Totholz, pos: new HexPosition(-1, 2) },
+        { contentType: Totholz, pos: new HexPosition(-4, 2) },
+        { contentType: Totholz, pos: new HexPosition(-4, 2) },
+        { contentType: Totholz, pos: new HexPosition(-4, 2) },
+        { contentType: Totholz, pos: new HexPosition(0, -4) },
+        { contentType: Totholz, pos: new HexPosition(-2, -1) },
+        { contentType: Totholz, pos: new HexPosition(-1, -2) },
+        { contentType: Totholz, pos: new HexPosition(0, 0) },
     ],
     effectDefinitions: [
         { effectType: Lava, pos: new HexPosition(2, -2), mustBeExecuted: (step) => step % 50 === 0 },
         { effectType: Gewitter, pos: new HexPosition(2, 1), mustBeExecuted: (step) => step % 12 === 0 },
-        { effectType: Regen, pos: new HexPosition(-3, 2), mustBeExecuted: (step) => step % 10 === 0 },
+        { effectType: Regen, pos: new HexPosition(-3, 2), mustBeExecuted: (step) => step % 20 === 0 },
         { effectType: Feuerball, pos: new HexPosition(-2, -1), mustBeExecuted: (step) => step % 30 === 0 },
         { effectType: Feuerball, pos: new HexPosition(0, 2), mustBeExecuted: (step) => step % 30 === 0 },
+        { effectType: Feuer3, pos: new HexPosition(-3, -1), mustBeExecuted: (step) => step % 40 === 0 },
+        { effectType: Feuer3, pos: new HexPosition(-1, 3), mustBeExecuted: (step) => step % 40 === 0 },
     ],
     levelID: 'lvl004',
-    creditStartingAmount: 450,
+    creditStartingAmount: 300,
 };
 
 export default lvl004;
