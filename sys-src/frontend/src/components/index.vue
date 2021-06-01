0f83960f8d74e61dc9933ed01d53f82ae591c8c6
Christian Piffel <c.piffel@oth-aw.de>
2021-06-02 00:24:38 +0200

    create login formular
    Former-commit-id: 2a84d90f828de620543fbd590cf047b986238343

--- sys-src/frontend/src/components/index.vue
+++ sys-src/frontend/src/components/index.vue
@@ -3,21 +3,37 @@
         <div class="main-menu">
             <logo />
             <div v-if="requiredMessage" class="text-danger">{{ $t('Please enter nickname') }}</div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <label for="nickname" class="input-group-text">{{ $t('Nickname') }}</label>
-                </div>
+
+            <div class="input-group mb-3 ">
+                <label for="nickname" class="input-group-text">{{$t("Nickname")}}</label>
                 <input id="nickname" v-model="nickname" class="form-control" @keyup.enter="setNickname" />
+
             </div>
+            <div class="input-group mb-3 " >
+                <label for="password" class="input-group-text">Passwort:</label>
+                <input type="password" id="Password" v-model="password" class="form-control"/>
+            </div>
+
             <div class="mb-3">
                 <button type="submit" class="btn btn-primary btn-block btn-lg" @click="setNickname">
                     {{ $t('log in') }}
                 </button>
             </div>
-            <hr class="mb-3" />
-            <div>
-                <router-link class="btn btn-primary btn-block btn-lg" to="/credits">{{ $t('Credits') }}</router-link>
+            <div class="d-flex justify-content-between">
+                <div class="mb-3">
+                    <a class="link-secondary" href="#">Neuen Account erstellen?</a>
+                </div>
+                <div class="mb-3">
+                    <a class="link-secondary" href="#">Passwort vergessen?</a>
+                </div>
             </div>
+
+
+            <hr class="mb-3" />
+
+
+            <router-link class="btn btn-primary btn-block btn-lg" to="/credits">{{ $t('Credits') }}</router-link>
+
         </div>
     </div>
 </template>
@@ -34,6 +50,7 @@ export default Vue.extend({
     data() {
         return {
             nickname: '',
+            password: '',
             requiredMessage: false,
         };
     },
