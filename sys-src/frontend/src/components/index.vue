036b88ecc9d093470b288e319c874db918c5b6c1
Piffel Christian <c.piffel@oth-aw.de>
2021-06-05 01:29:10 +0200

    define authentication requests + authentication service + validation for user login
    Former-commit-id: 89e3bb2416a5b9c94ef2d565a52df49943dc5a37
    Former-commit-id: ff338e494b17e84d2d54f6c760b48dc099654f3b

--- sys-src/frontend/src/components/index.vue
+++ sys-src/frontend/src/components/index.vue
@@ -1,29 +1,44 @@
 <template>
     <div class="main-menu-bg">
         <div class="main-menu">
-            <logo />
-            <div v-if="requiredMessage" class="text-danger">{{ $t('Please enter nickname') }}</div>
+            <logo  />
+            <form name="form" @submit.prevent="handleLogin">
 
             <div class="input-group mb-3 ">
+
                 <div class="input-group-prepend w-50">
-                    <label for="nickname" class="input-group-text w-100 ">{{$t("Nickname")}}</label>
+                    <label id="username" class="input-group-text w-100 ">Username</label>
 
                 </div>
-                <input id="nickname" v-model="nickname" class="form-control" @keyup.enter="setNickname" />
+
+                <input name="nickname" type="text" v-model="user.username"  v-validate="'required'"  id="nickname" class="form-control"  />
+
 
             </div>
+
             <div class="input-group mb-3 " >
                 <div class="input-group-prepend w-50">
                     <label for="password" class="input-group-text w-100 ">{{ $t('Password') }}</label>
                 </div>
-                <input type="password" id="Password" v-model="password" class="form-control"/>
+
+                <input name="password" type="password" id="Password" v-model="user.password" v-validate="'required'" class="form-control"/>
             </div>
 
+
             <div class="mb-3">
-                <button type="submit" class="btn btn-primary btn-block btn-lg" @click="setNickname">
+                <button type="submit"  class="btn btn-primary btn-block btn-lg" >
                     {{ $t('log in') }}
                 </button>
             </div>
+                <div class="input-group mb-3 ">
+                    <span class="alert-danger mb-3">{{errors.first('nickname')}}</span>
+                    <span class="alert-danger mb-3">{{errors.first('password')}}</span>
+                    <div v-if="message" class="alert alert-danger" role="alert">
+                        {{ message }}
+                    </div>
+                </div>
+
+
             <div class="d-flex justify-content-between">
                 <div class="mb-3">
                     <a class="link-secondary" href="#">{{ $t('Create new account?') }}</a>
@@ -38,37 +53,66 @@
 
 
             <router-link class="btn btn-primary btn-block btn-lg" to="/credits">{{ $t('Credits') }}</router-link>
-
+            </form>
         </div>
     </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import User from '@model/User';
-import logo from './logo.vue';
 
-export default Vue.extend({
-    components: {
-        logo,
-    },
-    data() {
-        return {
-            nickname: '',
-            password: '',
-            requiredMessage: false,
-        };
-    },
-    methods: {
-        setNickname: function () {
-            if (this.nickname !== '') {
-                const user = User.getInstance();
-                user.login(this.nickname);
-                this.$router.push('/world');
-            } else {
-                this.requiredMessage = true;
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+
+import logo from "./logo.vue";
+const Auth = namespace("Auth");
+
+@Component({
+    components:{
+        logo
+    }
+})
+export default class Login extends Vue {
+
+
+    private user: any = { username: "", password: "" };
+    private loading: boolean = false;
+    private message: string = "";
+
+    @Auth.Getter
+    private isLoggedIn!: boolean;
+
+    @Auth.Action
+    private login!: (data: any) => Promise<any>;
+
+    created() {
+        if (this.isLoggedIn) {
+            this.$router.push("/world");
+        }
+    }
+
+    handleLogin() {
+        this.loading = true;
+        this.$validator.validateAll().then((isValid : boolean) => {
+            if (!isValid) {
+                this.loading = false;
+                return;
             }
-        },
-    },
-});
+
+            if (this.user.username && this.user.password) {
+                this.login(this.user).then(
+                    () => {
+                        this.$router.push("/world");
+                    },
+                    (error:any) => {
+                        this.loading = false;
+                        this.message = error;
+                    }
+                );
+            }
+        });
+    }
+}
+
 </script>
--- sys-src/frontend/src/index.ts
+++ sys-src/frontend/src/index.ts
@@ -1,44 +1,53 @@
-import './scss/main.scss';
-import 'bootstrap';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueI18n from 'vue-i18n';
-import messages from './lang';
-import locale from './locale';
-import app from './app.vue';
-import index from './components/index.vue';
-import credits from './components/credits.vue';
-import level from './components/level.vue';
-import world from './components/world.vue';
-import User from './model/User';
+import "./scss/main.scss";
+import "bootstrap";
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VueI18n from "vue-i18n";
+import messages from "./lang";
+import locale from "./locale";
+import app from "./app.vue";
+import index from "./components/index.vue";
+import credits from "./components/credits.vue";
+import level from "./components/level.vue";
+import world from "./components/world.vue";
+import User from "./model/User";
+import VeeValidate from "vee-validate";
+import store from "@store/vuexStoreModules";
 
 Vue.use(VueRouter);
 Vue.use(VueI18n);
+Vue.config.productionTip = false;
+
+Vue.use(VeeValidate);
 
 const routes = [
-    { path: '/', component: index },
-    { path: '/credits', component: credits },
-    { path: '/world', component: world },
-    { path: '/level/:levelID', component: level, props: true },
+    { path: "/", component: index },
+    { path: "/credits", component: credits },
+    { path: "/world", component: world },
+    { path: "/level/:levelID", component: level, props: true },
 ];
 
 export const router = new VueRouter({
     routes,
-    mode: 'history',
+    mode: "history",
 });
 
 // Redirect to '/' when user is not logged in
 router.beforeEach((to, from, next) => {
-    if (User.getInstance().isLoggedIn() || to.path === '/' || to.path === '/credits') {
+    if (
+        User.getInstance().isLoggedIn() ||
+        to.path === "/" ||
+        to.path === "/credits"
+    ) {
         next();
     } else {
-        next('/');
+        next("/");
     }
 });
 
 const i18n = new VueI18n({
-    locale: 'de',
-    fallbackLocale: 'de',
+    locale: "de",
+    fallbackLocale: "de",
     messages,
 });
 
@@ -46,9 +55,10 @@ locale.setVueI18n(i18n);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const vm: Vue = new Vue({
-    el: '#app',
+    el: "#app",
     components: { app },
-    template: '<app/>',
+    template: "<app/>",
     router,
+    store,
     i18n,
 });
