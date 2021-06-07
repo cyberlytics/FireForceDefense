0d64b4fff47620f847ef25f21e526f8ed962e62b
christian piffel <c.piffel@oth-aw.de>
2021-06-07 13:34:00 +0200

    refactor code
    Former-commit-id: 86b1e066ef37933d5bab1893f06f830f7607831f

--- sys-src/frontend/src/services/user.service.ts
+++ sys-src/frontend/src/services/user.service.ts
@@ -1,43 +1,26 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8000/api/auth/";
+const API_URL = 'http://localhost:8000/api/auth/';
 
 export const userService = {
     login,
     logout,
 };
 
-async function login(username: string, password: string): Promise<any> {
-    const response = await axios.post(API_URL + "signin", {
+async function login(username: string, password: string): Promise<unknown> {
+    const response = await axios.post(API_URL + 'signin', {
         username,
         password,
     });
     if (response.data.accessToken) {
-        localStorage.setItem("user", JSON.stringify(response.data));
+        localStorage.setItem('user', JSON.stringify(response.data));
     }
     return response.data;
 }
 
 function logout(): void {
     // remove user from local storage to log user out
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
 }
 
-/*
-function handleResponse(response: any) {
-    return response.text().then((text: string) => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                logout();
-                location.reload();
-            }
 
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
-
-        return data;
-    });
-}
-*/
