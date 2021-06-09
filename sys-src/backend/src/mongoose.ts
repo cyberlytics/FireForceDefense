7012200009f3dd2ee0183d72b6a94d1b4fb98c88
Tobias Schotter <tschotter98@gmx.de>
2021-06-09 19:29:21 +0200

    formated code for pipeline
    Former-commit-id: 1a7ae6cfb56510b33b6384296eeddff0cfa71edd

--- sys-src/backend/src/mongoose.ts
+++ sys-src/backend/src/mongoose.ts
@@ -3,33 +3,30 @@ import mongoose from 'mongoose';
 let database: mongoose.Connection;
 
 export const connect = () => {
-    const url = 'mongodb://localhost:27017/'
+    const url = 'mongodb://localhost:27017/';
     const options = {
         useCreateIndex: true,
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+        useUnifiedTopology: true,
+    };
 
-    mongoose.connect(url, options,
-        (err) => {
-            if (err) throw err;
-        });
+    mongoose.connect(url, options, (err) => {
+        if (err) throw err;
+    });
 
     database = mongoose.connection;
 
-
-    database.once("open", async () => {
-        console.log("Connected to database: " + url);
+    database.once('open', async () => {
+        console.log('Connected to database: ' + url);
     });
-    database.on("error", () => {
-        console.log("Error connecting to database");
+    database.on('error', () => {
+        console.log('Error connecting to database');
     });
-}
+};
 
 export const disconnect = () => {
     if (!database) {
         return;
     }
-    mongoose.disconnect(err => {});
+    mongoose.disconnect((err) => {});
 };
-
