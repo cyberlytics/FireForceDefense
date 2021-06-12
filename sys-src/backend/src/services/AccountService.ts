5390da51eb09c21d477c682b59f32e8124e2370f
Philipp Stangl <p.stangl1@oth-aw.de>
2021-06-13 00:06:33 +0200

    Add authentication service
    Former-commit-id: b73508eeaa3e41fbb667af89aa2e0acc8bcae4c4
    Former-commit-id: e1de08475d4ffa300450cc9e76ae629a54539394

---
+++ sys-src/backend/src/services/AccountService.ts
@@ -0,0 +1,96 @@
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
+import { AccountModel, RefreshTokenModel } from '../Database';
+import type { Types } from 'mongoose';
+import type AccountDoc from 'Account';
+
+async function login(request: {
+    username: string;
+    email: string;
+    password: string;
+    ip: string;
+}): Promise<{ id: Types.ObjectId; username: string; email: string; jwtToken: string; refreshToken: string }> {
+    const account = await AccountModel.findOne({
+        $or: [{ username: request.username }, { email: request.email }],
+    });
+
+    if (!account || !bcrypt.compareSync(request.password, account.passwordHash)) {
+        throw 'Login failed';
+    }
+
+    // generate and save refresh token
+    const refreshToken = generateRefreshToken(account, request.ip);
+    await refreshToken.save();
+
+    // generate jwt
+    const accessToken = generateAccessToken(account);
+
+    return {
+        id: account.id,
+        username: account.username,
+        email: account.email,
+        jwtToken: accessToken,
+        refreshToken: refreshToken.token,
+    };
+}
+
+async function refreshToken(request: {
+    ip: string;
+    token: string;
+}): Promise<{ id: Types.ObjectId; username: string; email: string; jwtToken: string; refreshToken: string }> {
+    const refreshToken = await getRefreshToken(request.token);
+    const { account } = refreshToken;
+
+    // renew refresh token
+    const newRefreshToken = generateRefreshToken(account, request.ip);
+    refreshToken.revoked = Date.now();
+    refreshToken.revokedByIp = request.ip;
+    refreshToken.replacedByToken = newRefreshToken.token;
+
+    // save new refresh token
+    await refreshToken.save();
+    await newRefreshToken.save();
+
+    // return account details with tokens
+    return {
+        id: account.id,
+        username: account.username,
+        email: account.email,
+        jwtToken: generateAccessToken(account),
+        refreshToken: newRefreshToken.token,
+    };
+}
+
+function generateAccessToken(account: AccountDoc) {
+    return jwt.sign({ id: account.id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+}
+
+async function revokeToken(request: { token: string; ip: string }): Promise<void> {
+    const refreshToken = await getRefreshToken(request.token);
+
+    // revoke token
+    refreshToken.revoked = Date.now();
+    refreshToken.revokedByIp = request.ip;
+
+    await refreshToken.save();
+}
+
+async function getRefreshToken(token: string) {
+    const refreshToken = await RefreshTokenModel.findOne({ token: token }).populate('account');
+    if (!refreshToken || !refreshToken.isActive) throw 'Invalid Refresh Token!';
+    return refreshToken;
+}
+
+function generateRefreshToken(account: AccountDoc, ip: string) {
+    // refresh token expires after 24 hours
+    const expireyDelta = 24 * 60 * 60 * 1000;
+    return new RefreshTokenModel({
+        account: account.id,
+        token: crypto.randomBytes(40).toString('hex'),
+        expires: new Date(Date.now() + expireyDelta),
+        createdByIp: ip,
+    });
+}
+
+export { login, refreshToken, revokeToken };
