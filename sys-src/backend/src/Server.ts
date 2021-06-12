f69781a292a2c8cd50622e127c411329f8f4aecc
Philipp Stangl <p.stangl1@oth-aw.de>
2021-06-13 00:08:21 +0200

    Add cors and cookie-parser to server
    Former-commit-id: fe6b421626da90de2163e83578e60b16a7e77f49
    Former-commit-id: 1aa19a1fed22350508166de6f4097a12ed783cb5

--- sys-src/backend/src/Server.ts
+++ sys-src/backend/src/Server.ts
@@ -1,10 +1,11 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import path from 'path';
 import YAML from 'yamljs';
 import swaggerUi from 'swagger-ui-express';
-import AccountsController from './AccountsController';
-import GameController from './GameController';
-import { connect } from './mongoose';
+import cookieParser from 'cookie-parser';
+import AccountsController from './controllers/AccountsController';
+import GameController from './controllers/GameController';
 
 export default class Server {
     public app: Express;
@@ -12,30 +13,13 @@ export default class Server {
 
     constructor() {
         this.app = express();
-        this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            next();
-        });
+        this.app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+        this.app.use(cookieParser());
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.static(path.join(__dirname, '../static')));
-
-        const swaggerDocumentation = YAML.load(path.resolve(__dirname, '../swagger.yaml'));
-        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
-
         this.initControllers();
-
-        //temporary place for 'connect()' for testing purpose
-        connect();
-    }
-
-    registerAppGet(location: string | RegExp, func: (req: express.Request, res: express.Response) => void): void {
-        this.app.get(location, func);
-    }
-
-    registerAppPost(location: string | RegExp, func: (req: express.Request, res: express.Response) => void): void {
-        this.app.post(location, func);
+        this.serveSwaggerDocumentation();
     }
 
     start(): void {
@@ -51,5 +35,10 @@ export default class Server {
     private initControllers() {
         this.app.use('/', new AccountsController().router);
         this.app.use('/', new GameController().router);
+    }
+
+    private serveSwaggerDocumentation() {
+        const swaggerDocumentation = YAML.load(path.resolve(__dirname, '../swagger.yaml'));
+        this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
     }
 }
