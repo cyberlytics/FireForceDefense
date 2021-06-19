6778d20e0820bd005e254c0ed4a5b26894f1467c
Philipp <philipp.stangl95@gmail.com>
2021-06-19 17:31:46 +0200

    Update GameController
    Former-commit-id: 7371a9defa039e755cc73d043a44c28d6bed463f

--- sys-src/backend/src/controllers/GameController.ts
+++ sys-src/backend/src/controllers/GameController.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from 'express';
 import { Router } from 'express';
 import Joi from 'joi';
 import validateRequestSchema from '../middleware/RequestSchemaValidator';
+import * as gameService from '../services/GameServices';
 
 /**
  * API Handling for /game paths.
@@ -11,40 +12,80 @@ export default class GameController {
     public router = Router();
 
     constructor() {
-        this.router.get(`${this.path}/:id`, this.getLevelDataById);
-        this.router.delete(`${this.path}/:id`, this.deleteLevelDataById);
+        this.router.get(`${this.path}/:id`, this.getScoresById);
+        this.router.delete(`${this.path}/:id`, this.deleteScoresById);
 
-        this.router.post(`${this.path}/save`, this.dataSchema, this.saveLevelData);
+        this.router.post(`${this.path}/save`, this.scoresSchema, this.saveScores);
     }
 
-    private getLevelDataById = (req: Request, res: Response) => {
-        // TODO
-        return res.status(200).json({ message: 'Get level data by id' });
+    private getScoresById = (req: Request, res: Response, next: NextFunction) => {
+        const id = req.params.id;
+        gameService
+            .findScoreById(id)
+            .then((results) => {
+                return res.json({
+                    scoreData: results,
+                });
+            })
+            .catch(next);
     };
 
-    private deleteLevelDataById = (req: Request, res: Response) => {
-        // TODO
-        return res.status(200).json({ message: 'Delete level data by id' });
+    private deleteScoresById = (req: Request, res: Response, next: NextFunction) => {
+        const id = req.params.id;
+
+        gameService
+            .deleteScoreById(id)
+            .then((result) => {
+                res.json({
+                    deletedData: result,
+                    message: 'Scores deleted!',
+                });
+            })
+            .catch(next);
     };
 
-    //not final
-    private dataSchema = (req: Request, res: Response, next: NextFunction) => {
+    private saveScores = async (req: Request, res: Response, next: NextFunction) => {
+        const { username, level, stars, money, burnedFields, time } = req.body;
+
+        try {
+            if ((await gameService.userCheck(username, level)) === false) {
+                return gameService
+                    .createScore({ username, level, stars, money, burnedFields, time })
+                    .then((result) => {
+                        return res.json({
+                            scoreData: result,
+                            message: 'New data inserted',
+                        });
+                    })
+                    .catch(next);
+            } else {
+                return gameService
+                    .updateScore({ username, level, stars, money, burnedFields, time })
+                    .then((result) => {
+                        return res.json({
+                            scoreData: result,
+                            message: 'Old data updated',
+                        });
+                    })
+                    .catch(next);
+            }
+        } catch (error) {
+            return res.json({
+                message: error.message,
+                error,
+            });
+        }
+    };
+
+    private scoresSchema = (req: Request, res: Response, next: NextFunction) => {
         const schema = Joi.object({
-            user: Joi.object({
-                name: Joi.string().empty('').required(),
-            }),
-            date: Joi.date(),
+            username: Joi.string().required(),
             level: Joi.number().integer().required(),
-            stars: Joi.number().integer().required(),
+            stars: Joi.number().integer().max(3).required(),
             money: Joi.number().integer().required(),
-            timeUsed: Joi.string().empty('').required(),
-            burnedFields: Joi.string().empty('').required(),
+            burnedFields: Joi.number().integer().required(),
+            time: Joi.number().integer().required(),
         });
         validateRequestSchema(req, next, schema);
-    };
-
-    private saveLevelData = (req: Request, res: Response) => {
-        // TODO
-        return res.status(200).json({ message: 'Data saved' });
     };
 }
