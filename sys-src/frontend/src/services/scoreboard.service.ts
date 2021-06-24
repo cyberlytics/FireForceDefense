7d1b276b892edf5a9ba01c70f5f09371626cefa9
Tim Hoffmann <t.hoffmann1@oth-aw.de>
2021-06-24 15:32:23 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Former-commit-id: 67fd7e7c95986fc3305f0f9f17ec28b92676bb95

--- sys-src/frontend/src/services/scoreboard.service.ts
+++ sys-src/frontend/src/services/scoreboard.service.ts
@@ -10,43 +10,12 @@ export const scoreboardService = {
     getLevel4Data,
 };
 
-async function getLevelAllData(): Promise<unknown> {
-    const response = await axios.get(API_URL + '/game/scoreboard');
-    return response.data;
-}
-
-async function getLevel1Data(): Promise<unknown> {
-    const response = await axios.get(API_URL + '/game/scoreboard', {
+async function getData(level: string): Promise<unknown> {
+    const config = level === null || level === undefined ? {} : {
         params: {
-            level: 'lvl001',
+            level,
         },
-    });
-    return response.data;
-}
-
-async function getLevel2Data(): Promise<unknown> {
-    const response = await axios.get(API_URL + '/game/scoreboard', {
-        params: {
-            level: 'lvl002',
-        },
-    });
-    return response.data;
-}
-
-async function getLevel3Data(): Promise<unknown> {
-    const response = await axios.get(API_URL + '/game/scoreboard', {
-        params: {
-            level: 'lvl003',
-        },
-    });
-    return response.data;
-}
-
-async function getLevel4Data(): Promise<unknown> {
-    const response = await axios.get(API_URL + '/game/scoreboard', {
-        params: {
-            level: 'lvl004',
-        },
-    });
+    };
+    const response = await axios.get(API_URL + '/game/scoreboard', config);
     return response.data;
 }
