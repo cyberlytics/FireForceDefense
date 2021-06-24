77dc02ef5713fefb2fb8bfd2045a045c65e86479
Piffel Christian <c.piffel@oth-aw.de>
2021-06-24 18:22:51 +0200

    refactor in memory db
    Former-commit-id: 07555944346c2ab85b3456223d4144be766c6a72
    Former-commit-id: f5620149e618dc15e622f84071ba7dea93f95b27

--- sys-src/backend/src/__tests__/AccountControllerTest.ts
+++ sys-src/backend/src/__tests__/AccountControllerTest.ts
@@ -1,8 +1,8 @@
 import server from '../Server';
 import request from 'supertest';
-
 import { setupDB } from './databaseTestSetup';
-
+import {AccountModel} from "../models/Account";
+import {RefreshTokenModel} from "../models/RefreshToken";
 const s = new server();
 
 setupDB();
@@ -12,10 +12,26 @@ it('Should login user', async (done) => {
         email: 'dummy1@gmail.com',
         password: '123456',
     });
-
-    expect(res.body.username).toBeTruthy();
+    console.log(res.body);
+    expect(res.body.username)
     expect(res.body.email).toBeTruthy();
     expect(res.body.jwtToken).toBeTruthy();
+    done();
+});
+
+it('refresh token', async (done) => {
+    const resUser = await request(s.app).post('/accounts/register').send({
+        username: 'dummy1',
+        email: 'dummy1@gmail.com',
+        password: '123456',
+    });
+
+    const token = await RefreshTokenModel.findOne().populate('account');
+    console.log(token)
+    const res = await request(s.app).post('/accounts/refresh-token').send({
+
+    }).set("Cookie", [`refreshToken=${token.token}`]);
+console.log(res.body)
     done();
 });
 
@@ -26,7 +42,7 @@ it('Should save user to database', async (done) => {
         password: '123456',
         confirmPassword: '123456',
     });
-
+    console.log(res.body)
     expect(res.body.username).toBeTruthy();
     expect(res.body.email).toBeTruthy();
     expect(res.body.jwtToken).toBeTruthy();
