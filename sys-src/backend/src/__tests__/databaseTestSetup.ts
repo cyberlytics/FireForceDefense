31052ffa380bebaa1047b4d1c7de6a854bb166eb
Christian Piffel <c.piffel@oth-aw.de>
2021-06-24 18:25:35 +0200

    refactor setup memory db
    Former-commit-id: 5b1bef1f83f711943ec194ca24ccc33740e093d9

--- sys-src/backend/src/__tests__/databaseTestSetup.ts
+++ sys-src/backend/src/__tests__/databaseTestSetup.ts
@@ -1,32 +1,30 @@
-import mongoose, { CallbackError } from 'mongoose';
+// import mongoose, { CallbackError } from 'mongoose';
 import request from 'supertest';
 import server from '../Server';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import type {CallbackError} from "mongoose";
+import mongoose from 'mongoose';
 const mongod = new MongoMemoryServer();
 const s = new server();
 jest.useFakeTimers();
 
 function setupDB(): void {
     // Connect to Mongoose
+    beforeAll(async (done) => {
 
-    beforeAll(async () => {
         const uri = await mongod.getUri();
         await mongoose.disconnect();
         //connects to the in memory database for testing purposes
         await mongoose.connect(
             uri,
-            { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+            {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},
             (err: CallbackError) => {
                 if (err) {
                     console.error(err);
                     process.exit(1);
                 }
-            },
+            }
         );
-
-        afterAll(async () => {
-            await mongoose.disconnect();
-        });
         // Seed the database with users
         await request(s.app).post('/accounts/register').send({
             username: 'dummy1',
@@ -40,8 +38,17 @@ function setupDB(): void {
             password: '1234567',
             confirmPassword: '1234567',
         });
-    });
+        done();
+    }   );
+
+        afterAll(async (done) => {
+            await mongoose.disconnect();
+            done();
+        });
+
+
 }
+
 
 it('should setup database ', function () {});
 export { setupDB };
