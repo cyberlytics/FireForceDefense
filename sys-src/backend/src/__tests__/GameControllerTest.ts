2d195c615d1bf2d4023253e6e6e218ed52a1a26a
Christian Piffel <c.piffel@oth-aw.de>
2021-06-26 15:34:45 +0200

    delete databaseTestSetup.ts because its causes unknown error
    Former-commit-id: aad871efe4617fc45f642510fd5efdfcf4bd51ee

--- sys-src/backend/src/__tests__/GameControllerTest.ts
+++ sys-src/backend/src/__tests__/GameControllerTest.ts
@@ -1,9 +1,46 @@
 import server from '../Server';
 import request from 'supertest';
-import { setupDB } from './databaseTestSetup';
-const s = new server();
 
-setupDB();
+import mongoose, { CallbackError } from 'mongoose';
+const s = new server();
+import { MongoMemoryServer } from 'mongodb-memory-server';
+const mongod = new MongoMemoryServer();
+jest.useFakeTimers();
+
+beforeAll(async (done) => {
+    const uri = await mongod.getUri();
+    await mongoose.disconnect();
+    //connects to the in memory database for testing purposes
+    await mongoose.connect(
+        uri,
+        { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+        (err: CallbackError) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        },
+    );
+    // Seed the database with users
+    await request(s.app).post('/accounts/register').send({
+        username: 'dummy1',
+        email: 'dummy1@gmail.com',
+        password: '123456',
+        confirmPassword: '123456',
+    });
+    await request(s.app).post('/accounts/register').send({
+        username: 'dummy2',
+        email: 'dummy2@gmail.com',
+        password: '1234567',
+        confirmPassword: '1234567',
+    });
+    done();
+});
+
+afterAll(async (done) => {
+    await mongoose.disconnect();
+    done();
+});
 
 it('Set score', async (done) => {
     const resSetUser = await request(s.app).post('/game/score').send({
