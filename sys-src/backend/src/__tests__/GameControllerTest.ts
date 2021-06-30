1f74ac3f61aabb7b63ab6d7149e7ec0912dce06e
Christian Piffel <c.piffel@oth-aw.de>
2021-06-30 20:13:37 +0200

    fix test error
    Former-commit-id: 9f8f081c978891c8a57301eca35158623d87ff43

--- sys-src/backend/src/__tests__/GameControllerTest.ts
+++ sys-src/backend/src/__tests__/GameControllerTest.ts
@@ -1,17 +1,44 @@
 import server from '../Server';
 import request from 'supertest';
-const s = new server();
-import { closeDatabase, connect } from '../databaseHandler';
 
+import mongoose, { CallbackError } from 'mongoose';
+const s = new server();
+import { MongoMemoryServer } from 'mongodb-memory-server';
+const mongod = new MongoMemoryServer();
 jest.useFakeTimers();
 
 beforeAll(async (done) => {
-    await connect();
+    const uri = await mongod.getUri();
+    await mongoose.disconnect();
+    //connects to the in memory database for testing purposes
+    await mongoose.connect(
+        uri,
+        { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+        (err: CallbackError) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        },
+    );
+    // Seed the database with users
+    await request(s.app).post('/accounts/register').send({
+        username: 'dummy1',
+        email: 'dummy1@gmail.com',
+        password: '123456',
+        confirmPassword: '123456',
+    });
+    await request(s.app).post('/accounts/register').send({
+        username: 'dummy2',
+        email: 'dummy2@gmail.com',
+        password: '1234567',
+        confirmPassword: '1234567',
+    });
     done();
 });
 
 afterAll(async (done) => {
-    await closeDatabase();
+    await mongoose.disconnect();
     done();
 });
 
